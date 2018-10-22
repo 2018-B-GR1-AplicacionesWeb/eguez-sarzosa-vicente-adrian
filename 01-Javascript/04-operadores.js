a40be6d2d3892016e1dd80d963703e822eff9406
Adrian Eguez <eadepto@hotmail.com>
2018-10-22 09:32:35 -0500

    funciones anonimas

---
+++ 01-Javascript/04-operadores.js
@@ -0,0 +1,32 @@
+function ejemplo() {
+}
+
+var ejemploDos = function () {
+}; // Anonymous function
+
+var adrian = {
+    trabajar: function () {// Anonymous function
+        return 'Trabajando';
+    }
+};
+
+var arregloFunciones = [function () {// Anonymous function
+
+}];
+
+
+
+
+
+// operadores
+
+var arregloDeNombres = ['A', 'b', 'C'];
+
+arregloDeNombres.forEach(
+    function () {
+
+    }
+);
+
+
+
