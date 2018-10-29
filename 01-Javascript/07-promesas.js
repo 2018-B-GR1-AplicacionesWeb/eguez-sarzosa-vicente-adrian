88030a80ee1b1ffae249b9eaf2825862c1c5a6b8
Adrian Eguez <eadepto@hotmail.com>
2018-10-29 10:42:37 -0500

    promesas

---
+++ 01-Javascript/07-promesas.js
@@ -0,0 +1,49 @@
+// 07-promesas.js
+
+const fs = require('fs');
+
+const nuevaPromesaLectura = new Promise(
+    (resolve, reject) => {
+        fs.readFile('06-texto.txt', 'utf-8',
+            (err, contenidoArchivo) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(contenidoArchivo);
+                }
+            });
+    }
+);
+
+
+const nuevaPromesaEscritura = (contenidoLeido) => {
+    return new Promise(
+        (resolve, reject) => {
+
+            const contenido = contenidoLeido ? contenidoLeido + 'Otro ola' : 'Otro ola';
+
+            fs.writeFile('06-texto.txt', contenido,
+                (err,) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve('LO QUE SEA');
+                    }
+                });
+        }
+    );
+};
+
+nuevaPromesaLectura
+    .then(
+        (resultadoOk) => {
+            console.log('Todo bien', resultadoOk);
+
+        }
+    )
+    .catch(
+        (resultadoError) => {
+            console.log('Algo malo paso', resultadoError);
+        }
+    );
+
