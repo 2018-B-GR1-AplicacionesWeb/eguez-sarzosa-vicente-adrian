a0a39de949961df95027f06de8e601db686837d5
Adrian Eguez <eadepto@hotmail.com>
2019-01-14 09:42:03 -0500

    express session

--- 03-http/videos/src/main.ts
+++ 03-http/videos/src/main.ts
@@ -4,9 +4,22 @@ import {AppModule} from './app.module';
 // const http_server = require('http-server'); // JS
 import * as cookieParser from 'cookie-parser';
 import * as ejs from 'ejs';
+import * as session from 'express-session';
+
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    app.use(
+        session({
+            // name: 'server-session-id',
+            secret: 'No sera de tomar un traguito',
+            resave: false,
+            saveUninitialized: true,
+            cookie: {secure: false}
+        })
+    );
+
     app.use(cookieParser(
         'me gustan los tacos', // secreto
         {  // opciones
